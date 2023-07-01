50ac68dac1388979420de61755c6a492210daa94
Tuan Phan Manh <tuanphanmanh86@gmail.com>
2023-07-01 14:55:51 +0700

    End of section 2

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,11 @@
+namespace API.Entities
+{
+    public class AppUser
+    {
+        public AppUser(int id, string userName) 
+        {
+        }
+        public int Id {get; set;}
+        public string UserName {get; set;}
+    }
+}
