2907d464ccfba8f4eeb7eb32f6ac766770a93b6f
Tuan Phan Manh <tuanphanmanh86@gmail.com>
2023-07-06 14:54:24 +0700

    End of section 3

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -2,9 +2,6 @@ namespace API.Entities
 {
     public class AppUser
     {
-        public AppUser(int id, string userName) 
-        {
-        }
         public int Id {get; set;}
         public string UserName {get; set;}
     }
--- API/Startup.cs
+++ API/Startup.cs
@@ -30,12 +30,10 @@ namespace API
             services.AddDbContext<DataContext>(options =>
             {
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
+
             });
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-            });
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,13 +42,13 @@ namespace API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
             }
 
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
